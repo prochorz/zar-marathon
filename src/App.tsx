cecc91a8e3bf6dbacc6ab2aa991e1959b227d911
prochorsimon <Never7umer>
2020-11-03 15:33:31 +0200

    homework 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import cx from 'classnames';
 
+import Header from './components/header';
+import Footer from './components/footer';
+
 import Style from './App.module.scss';
 
 const App = () => {
-  return <div className={cx(Style.header, 'color')}>Hi</div>;
+  return (
+    <div className={cx(Style.wrapper)}>
+      <div className={cx(Style.content)}>
+        <Header />
+      </div>
+      <div className={cx(Style.footer)}>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default App;
